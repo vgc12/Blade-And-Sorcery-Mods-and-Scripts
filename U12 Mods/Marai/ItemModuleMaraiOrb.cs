32d7d8ea018cc0277f7b43bb6a562df7a8f8a425
Zaid <vgc4321@gmail.com>
2023-04-04 00:01:11 -0500

    Updated Marais Executioner's Sword to U12

---
+++ U12 Mods/Marai/ItemModuleMaraiOrb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThunderRoad;
+using UnityEngine;
+
+namespace Marai
+{
+
+    public class ItemModuleMaraiOrb : ItemModule
+    {
+        public string OrbRef, FirstRuneRef, SecondRuneRef, RuneTarget, DefaultLookPos;
+        public bool consumeMana;
+        public override void OnItemLoaded(Item item)
+        {
+            base.OnItemLoaded(item);
+            item.gameObject.AddComponent<MaraiOrb>();
+            item.gameObject.AddComponent<MaraiOrbFX>();
+        }
+    }
+}
